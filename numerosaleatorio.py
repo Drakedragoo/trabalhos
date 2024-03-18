7b1db0601e589f732cb88961dcde3b5b06633a80
Drakedragoo <163123866+Drakedragoo@users.noreply.github.com>
2024-03-18 12:04:19 -0300

    Add files via upload

---
+++ numerosaleatorio.py
@@ -0,0 +1,6 @@
+import random
+numero_minimo=int(input("digiite o valor minimo"))
+numero_maximo=int(input("digiite o valor maximo"))
+
+numero_aletorio=random.randint(numero_minimo,numero_maximo)
+print("o numero aleatorio é", numero_aletorio)
